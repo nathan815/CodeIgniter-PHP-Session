8224f79c50830d0d79c4c49cfe3bd863817d3178
Sergio <Alphasquare@users.noreply.github.com>
2014-06-23 01:27:23 -0400

    Changed some wording

--- php_session.php
+++ php_session.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!defined('BASEPATH')) exit( 'No direct script access allowed' );
+if(!defined('BASEPATH')) exit( 'No direct script access allowed, please allow it for proper functionality of this script.' );
 
 class PHP_session {
 
